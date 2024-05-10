651bb81512be56ba7206c7377aeb867cb25616b3
Mario <arminimario7@gmail.com>
2024-05-10 20:09:44 +0200

    added reporting manager tests

--- test/ReportingManager.ts
+++ test/ReportingManager.ts
@@ -22,6 +22,8 @@ describe("Reporting Manager", function () {
 
     let reportingManagerContract: any;
     let user: any;
+    const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+    const FAKE_ADDRESS = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F";
 
     beforeEach(async function () {
         const { reportingManagerContract: reportingManager, user: u1 } = await loadFixture(deploySocialMediaPlatform);
@@ -32,7 +34,30 @@ describe("Reporting Manager", function () {
 
     describe("Adding moderator", function () {
         it("Should add a moderator", async function () {
-            expect(await reportingManagerContract._addModerator)
+            await expect(reportingManagerContract._addModerator(user.address)).not.to.be.reverted;
+        });
+
+        it("Should not add zero address as moderator", async function () {
+            await expect(reportingManagerContract._addModerator(ZERO_ADDRESS)).to.be.revertedWith("Cannot add zero address");
+        });
+    });
+
+    describe("Checking moderator", function () {
+        it("Should check a moderator", async function () {
+            await reportingManagerContract._addModerator(user.address);
+            expect(await reportingManagerContract._isModerator(user.address)).to.be.true;
+        });
+    });
+
+    describe("Removing moderator", function () {
+        it("Should remove a moderator", async function () {
+            await reportingManagerContract._addModerator(user.address);
+            await reportingManagerContract._removeModerator(user.address);
+            expect(await reportingManagerContract._isModerator(user.address)).to.be.false;
+        });
+
+        it("Should not remove a moderator due to invalid address", async function () {
+            await expect(reportingManagerContract._removeModerator(FAKE_ADDRESS)).to.be.revertedWith("Address is not a moderator");
         });
     });
 });
